5e7be47a79e77f6b65cb8b8d26ea595df68d9efc
SvetaK7 <atevs7@inbox.ru>
2023-03-31 16:17:10 +0300

    fix: hw4 with autotests

--- src/p2-homeworks/h5-rrd-v6/pages/PreJunior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/PreJunior.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 // import HW1 from '../../h1/HW1'
 // import HW2 from '../../h2/HW2'
 // import HW3 from '../../h3/HW3'
-import HW4 from '../../h4/HW4'
+// import HW4 from '../../h4/HW4'
 import HW6 from "../../h6/HW6";
 import HW1 from "../../hw01/HW1";
 import HW2 from "p2-homeworks/hw02/HW2";
 import HW3 from "p2-homeworks/hw03/HW3";
+import HW4 from "p2-homeworks/hw04/HW4";
 
 function PreJunior() {
     return (
@@ -17,6 +18,7 @@ function PreJunior() {
           <HW2/>
             {/*<HW3/>*/}
             <HW3/>
+            {/*<HW4/>*/}
             <HW4/>
             <HW6/>
 
