a5ee0e4a7f556e3ee779b749fe4c63fc531549e4
SvetaK7 <atevs7@inbox.ru>
2023-04-05 21:36:50 +0300

    fix: hw7 with autotests

--- src/p2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -36,7 +36,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
       if (onChangeOption) {
-        onChangeOption(e.currentTarget.value)
+        onChangeOption(+e.currentTarget.value)
       }
     }
 
