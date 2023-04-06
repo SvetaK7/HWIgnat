8a10fbae0ff2989947a02a4bc534bbc49d3faccc
SvetaK7 <atevs7@inbox.ru>
2023-04-06 21:50:25 +0300

    fix: hw9 with autotests

---
+++ src/p2-homeworks/hw09/Clock.tsx
@@ -0,0 +1,93 @@
+import React, {useState} from 'react'
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import {restoreState} from '../hw06/localStorage/localStorage'
+import s from './Clock.module.css'
+
+function Clock() {
+    const [timerId, setTimerId] = useState<number | undefined>(undefined)
+    // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
+    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
+    const [show, setShow] = useState<boolean>(false)
+
+    const start = () => {
+        stop();
+        const id: number = window.setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+        setTimerId(id)
+    }
+
+    const stop = () => {
+        clearInterval(timerId)
+        setTimerId(undefined)
+    }
+
+    const onMouseEnter = () => {
+        setShow(true)
+
+    }
+    const onMouseLeave = () => {
+        setShow(false)
+    }
+
+    const stringTime = date?.toLocaleTimeString();
+    const stringDate = date?.toLocaleDateString();
+
+    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+
+    let formatter = new Intl.DateTimeFormat("en-US", {weekday: "long"});
+    let formatter2 = new Intl.DateTimeFormat("en-US", {month: "long"});
+
+    const stringDay = formatter.format(date);
+    const stringMonth = formatter2.format(date);
+
+    return (
+        <div className={s.clock}>
+            <div
+                id={'hw9-watch'}
+                className={s.watch}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+            >
+                <span id={'hw9-day'}>{stringDay}</span>,{' '}
+                <span id={'hw9-time'}>
+                    <strong>{stringTime}</strong>
+                </span>
+            </div>
+
+            <div id={'hw9-more'}>
+                <div className={s.more}>
+                    {show ? (
+                        <>
+                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+                            <span id={'hw9-date'}>{stringDate}</span>
+                        </>
+                    ) : (
+                        <>
+                            <br/>
+                        </>
+                    )}
+                </div>
+            </div>
+
+            <div className={s.buttonsContainer}>
+                <SuperButton
+                    id={'hw9-button-start'}
+                    disabled={!!timerId}
+                    onClick={start}
+                >
+                    start
+                </SuperButton>
+                <SuperButton
+                    id={'hw9-button-stop'}
+                    disabled={!timerId}
+                    onClick={stop}
+                >
+                    stop
+                </SuperButton>
+            </div>
+        </div>
+    )
+}
+
+export default Clock
