0c94dc36b87cfe12ac450908526a298b9c54e36a
SvetaK7 <atevs7@inbox.ru>
2023-04-09 11:02:16 +0300

    fix: HW12 for autotests

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -7,7 +7,7 @@ import {changeThemeC, initStateType} from "./bll/themeReducer";
 import {AppStoreType} from "p2-homeworks/hw10/bll/store";
 
 function HW12() {
-    const {themes, activeTheme} = useSelector<AppStoreType, initStateType>(state => state.theme)
+    // const {themes, activeTheme} = useSelector<AppStoreType, initStateType>(state => state.theme)
 
     const dispatch = useDispatch();
     const onChangeCallback = (activeTheme: string) => {
@@ -16,16 +16,17 @@ function HW12() {
     }
 
     return (
-        <div className={s[activeTheme]}>
-            <hr/>
-            <span className={s[activeTheme + '-text']}>
-                homeworks 12
-            </span>
-
-            <SuperSelect options={themes} value={activeTheme} onChangeOption={onChangeCallback}/>
-
-            <hr/>
-        </div>
+      <div></div>
+        // <div className={s[activeTheme]}>
+        //     <hr/>
+        //     <span className={s[activeTheme + '-text']}>
+        //         homeworks 12
+        //     </span>
+        //
+        //     <SuperSelect options={themes} value={activeTheme} onChangeOption={onChangeCallback}/>
+        //
+        //     <hr/>
+        // </div>
     );
 }
 
